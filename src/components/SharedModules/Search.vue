16685ad3f803935dbf5d6586a3c6eeb507ba9607
Kamal Johnson <kamaljohnson12345@gmail.com>
2021-05-29 05:14:20 +0530

    :art: Change endpoint local -> heroku

--- src/components/SharedModules/Search.vue
+++ src/components/SharedModules/Search.vue
@@ -178,7 +178,7 @@ export default {
                     filters: this.filters,
                 })
                 }
-                fetch('http://127.0.0.1:5000/search', requestOptions)
+                fetch('https://blooming-basin-03878.herokuapp.com/search', requestOptions)
                 .then(res => res.json())
                 .then((data) => {
                     console.log("data : " + data['result'])
